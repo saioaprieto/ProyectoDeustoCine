96b313ea7cd0b717e8f34a8724182c175523497b
Saioa Prieto <saioapri2006@gmail.com>
2025-11-08 16:35:47 +0100

    he cambiado el nombre de la ventana

--- proyectoDeustoCine/src/proyectoDeustoCine/VentanaTrabajador.java
+++ proyectoDeustoCine/src/proyectoDeustoCine/VentanaTrabajador.java
@@ -30,7 +30,7 @@ public class VentanaTrabajador extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public VentanaEmpleado() {
+	public VentanaTrabajador() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
