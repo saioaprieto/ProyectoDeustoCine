3a8ba033a8914067371c417d0fe9483bb03138c1
Saioa Prieto <saioapri2006@gmail.com>
2025-11-08 16:31:54 +0100

    He cambiado el nombre de la ventanaEMpleado

--- proyectoDeustoCine/src/proyectoDeustoCine/VentanaTrabajador.java
+++ proyectoDeustoCine/src/proyectoDeustoCine/VentanaTrabajador.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class VentanaEmpleado extends JFrame {
+public class VentanaTrabajador extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class VentanaEmpleado extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaEmpleado frame = new VentanaEmpleado();
+					VentanaTrabajador frame = new VentanaTrabajador();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class VentanaEmpleado extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public VentanaEmpleado() {
+	public VentanaTrabajador() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
