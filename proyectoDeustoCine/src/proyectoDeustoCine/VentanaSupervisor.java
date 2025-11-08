09f6c3d9a3be6d0d389fd003716a72973ec712df
m.gildesanvicente <m.gildesanvicente@SS24-60F1997600>
2025-11-08 16:50:37 +0100

    diseñi supervisor

--- proyectoDeustoCine/src/proyectoDeustoCine/VentanaSupervisor.java
+++ proyectoDeustoCine/src/proyectoDeustoCine/VentanaSupervisor.java
@@ -5,11 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class VentanaSupervisor extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblTitulo;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -37,6 +47,44 @@ public class VentanaSupervisor extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelNorte = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panelNorte.getLayout();
+		flowLayout.setVgap(10);
+		contentPane.add(panelNorte, BorderLayout.NORTH);
+		
+		lblTitulo = new JLabel("");
+		panelNorte.add(lblTitulo);
+		
+		JPanel panelSur = new JPanel();
+		contentPane.add(panelSur, BorderLayout.SOUTH);
+		panelSur.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panel = new JPanel();
+		panelSur.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panelSur.add(panel_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		panel_1.add(btnNewButton);
+		
+		JPanel panelDerecho = new JPanel();
+		contentPane.add(panelDerecho, BorderLayout.EAST);
+		panelDerecho.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		panelDerecho.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panelDerecho.add(btnNewButton_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
 	}
 
 }
